48a2daaaf91548cef3f96d4bf500a13f042d6fbf
Shubham Singh <shubhamsr08@gmail.com>
2024-08-28 02:00:09 +0530

    Some Changes

--- client/src/api.js
+++ client/src/api.js
@@ -1,3 +1,3 @@
-const BASE_API = "http://localhost:5000";
-//const BASE_API = '';
+//const BASE_API = "http://localhost:5000";
+const BASE_API = "https://echallan-3z8u.onrender.com";
 export default BASE_API;
